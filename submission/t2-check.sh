e0e212ffeb28abd6438d740bebd8246816179f07
rishabh <dahalerishabh1@gmail.com>
2020-09-17 00:29:23 +0530

    T2 testing 2

--- submission/t2-check.sh
+++ submission/t2-check.sh
@@ -1,13 +1,13 @@
 instance=('../instances/i-1.txt')
 algorithms=('thompson-sampling' 'thompson-sampling-with-hint')
-horizon=(100 400 1000)
+horizon=(1000 2000 5000)
 randomSeeds=()
 
 i=1
 
 for in in "${instance[@]}"; do
   for al in "${algorithms[@]}"; do
-    for rs in {0..49}; do
+    for rs in {0..4}; do
       for hz in "${horizon[@]}"; do
         echo "Test $i"
         python bandit.py --instance "$in" --algorithm "$al" --randomSeed "$rs" --epsilon 0.02 --horizon "$hz"
